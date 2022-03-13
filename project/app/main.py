e44fe22baebbd3b78474955e119a19dff7fec0fd
jesushledon@gmail.com <jesushledon@gmail.com>
2022-03-13 14:15:01 -0600

    Now database is connected to alembic, but us ont async yet

--- project/app/main.py
+++ project/app/main.py
@@ -8,12 +8,15 @@ from .settings import APPSettings, get_settings
 # description
 from .constants import PROJECT_DESCRIPTION, APP_TITLE
 # database
-from app.database import Base, engine   
+from app.database import  engine   
 # model
 from app.models import Company
 
+from app.models import Base
+
 app = FastAPI(title=APP_TITLE, description=PROJECT_DESCRIPTION)
 
+Base.metadata.create_all(bind=engine)
 
 @app.post("/aggregate/{symbol}")
 async def aggregate_symbol(symbol: str):
