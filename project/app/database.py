03fbb09b548a5bc800a124b86be178f3341b8555
jesushledon@gmail.com <jesushledon@gmail.com>
2022-03-13 12:42:38 -0600

    fixing a bad import

--- project/app/database.py
+++ project/app/database.py
@@ -1,7 +1,5 @@
-from turtle import pd
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 # settings
